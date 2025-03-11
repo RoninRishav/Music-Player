08b87dbce480986a33d99702115acb8450d82f7f
CodeRishav <128290247+CodeRishav@users.noreply.github.com>
2025-03-11 18:30:18 +0530

    Opening Screen Created

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 450,
+    resizable: false,
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+/// Open Window
+
+app.whenReady().then(() => {
+
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+/// Close Window
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
